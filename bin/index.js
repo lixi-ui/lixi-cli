cc411c0919f0fa41f5c669abd186bdad643bef85
niker <lingyp@yisa.com>
2020-05-06 07:39:08 +0800

    commit by ling

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const program = require('commander');
+
+program
+    .command('create <projectName>')
+    .action((projectName) => {
+        require('../lib/command/create.js')(projectName);
+    });
+
+program.parse(process.argv);   
